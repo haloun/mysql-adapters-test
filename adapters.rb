8005281efb2dc9819f1a9e2c5dd5b16ad248089d
Petr Haloun <petrhaloun@gmail.com>
2014-04-21 22:41:59 +0200

    output to csv file

--- adapters.rb
+++ adapters.rb
@@ -1,5 +1,6 @@
 require 'net/http' # gem for http communication (included in ruby)
 require 'json'     # gem for parsing json (gem install json)
+require 'csv'	   # for saving results to csv for further use
 
 # tested database adapters
 require 'mysql'   		# gem install mysql
@@ -14,11 +15,57 @@ require 'active_record'  # gem install activerecord
 @database = 'integrace'
 @adapter = 'mysql2'
 
+=begin
+# Class for active record ORM mapping
+class Tvshow < ActiveRecord::Base  
+end
+=end
+	
+# Methods for connection to the database through various adapters
+def mysql_adapter(record)
+	begin
+		db = Mysql.new(@server, @username, @password, @database)
+		db.query("INSERT INTO tvshows (show_name) values ('#{record}')")
+	ensure
+		db.close if db
+	end
+end
+
+def mysql2_adapter(record)
+	begin
+		db = Mysql2::Client.new(:host => @server, :username => @username, :password => @password, :database => @database)
+		db.query("INSERT INTO tvshows (show_name) values ('#{record}')")
+	ensure
+		db.close if db
+	end
+end
+
+def dbi_adapter(record)
+	begin
+		db = DBI.connect("DBI:Mysql:integrace:localhost", "root", "")
+		db.do("INSERT INTO tvshows (show_name) values ('#{record}')")
+	ensure
+		db.disconnect if db
+	end
+end
+
+def active_record_adapter(record)
+	begin
+		db = ActiveRecord::Base.establish_connection(:adapter => @adapter, :host => @server, :database => @database) 
+		#Tvshow.create(:movie_name => show_name)
+		db.connection.execute("INSERT INTO tvshows (show_name) values ('#{record}')")
+	ensure
+		db.disconnect! if db
+	end
+end
+
+# ============== settings ==============
 # variables for repetitions
 no_of_repetitions = 2
 no_of_connections = 1000
 # Possibilities 'mysql_adapter', 'mysql2_adapter', 'dbi_adapter', 'active_record_adapter'
-adapter = 'active_record_adapter'
+adapter = 'mysql_adapter'
+# ============== end of settings ==============
 
 # Fetches JSON string with tv shows,
 # parse it to ruby hash and then creates array with 10 movies
@@ -42,53 +89,19 @@ no_of_repetitions.times do
 				send(adapter.to_s, show)
 			end
 		end
-		finish = Time.now
+	finish = Time.now
 
+	puts (finish - start)
 	results << (finish - start)
 end
 
-puts results
-
-=begin
-# Class for active record ORM mapping
-class Tvshow < ActiveRecord::Base  
-end
-=end
-	
-# Methods for connection to the database through various adapters
-def mysql_adapter(record)
-	begin
-		db = Mysql.new(@server, @username, @password, @database)
-		db.query("INSERT INTO shows (show_name) values ('#{record}')")
-	ensure
-		db.close if db
+# Saves results to a csv file
+File.open('results.csv', 'w+') do |file|
+	results.each do |result|
+		file.puts result.to_s
 	end
 end
 
-def mysql2_adapter(record)
-	begin
-		db = Mysql2::Client.new(:host => @server, :username => @username, :password => @password, :database => @database)
-		db.query("INSERT INTO shows (show_name) values ('#{record}')")
-	ensure
-		db.close if db
-	end
-end
 
-def dbi_adapter(record)
-	begin
-		db = DBI.connect("DBI:Mysql:integrace:localhost", "root", "")
-		db.do("INSERT INTO shows (show_name) values ('#{record}')")
-	ensure
-		db.disconnect if db
-	end
-end
 
-def active_record_adapter(record)
-	begin
-		db = ActiveRecord::Base.establish_connection(:adapter => @adapter, :host => @server, :database => @database) 
-		#Tvshow.create(:movie_name => show_name)
-		db.connection.execute("INSERT INTO tvshows (show_name) values ('#{record}')")
-	ensure
-		db.disconnect! if db
-	end
-end
+
