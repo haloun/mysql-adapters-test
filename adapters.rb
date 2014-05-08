ed35736033e8a773249b2fd5161391ba5d87e633
Petr Haloun <petrhaloun@gmail.com>
2014-05-09 00:33:29 +0200

    final version for tests

--- adapters.rb
+++ adapters.rb
@@ -1,6 +1,4 @@
-require 'net/http' # gem for http communication (included in ruby)
-require 'json'     # gem for parsing json (gem install json)
-require 'csv'	   # for saving results to csv for further use
+require 'csv'	   # for saving results to csv
 
 # tested database adapters
 require 'mysql'   		# gem install mysql
@@ -12,60 +10,192 @@ require 'active_record'  # gem install activerecord
 @server = 'localhost'
 @username = 'root'
 @password = ''
-@database = 'integrace'
-@adapter = 'mysql2'
+@database = 'soap'
+@adapter = 'mysql2' # param used just for active record
 
-=begin
+
 # Class for active record ORM mapping
-class Tvshow < ActiveRecord::Base  
+class Customer < ActiveRecord::Base
+	has_many :items
 end
-=end
+
+class Customers_item < ActiveRecord::Base
+end
+
+class Item < ActiveRecord::Base
+	has_many :customers
+end
+
 	
 # Methods for connection to the database through various adapters
-def mysql_adapter(record)
-	begin
-		db = Mysql.new(@server, @username, @password, @database)
-		db.query("INSERT INTO tvshows (show_name) values ('#{record}')")
-	ensure
-		db.close if db
-	end
+def mysql_adapter()
+
+		# create the object and establish connection if doesn't exist
+		if ($db).nil?
+			$db = Mysql.new(@server, @username, @password, @database)
+		end
+=begin SECOND TEST
+		$db.query("INSERT INTO customers (email, name) values ('test@test.com', 'pepaX');")
+		$db.query("UPDATE customers SET email='test1@test.com', name='pepaY' WHERE name='pepaX';")
+		$db.query("SELECT * FROM customers WHERE name='pepaY';")
+		$db.query("DELETE FROM customers WHERE name='pepaY';")
+=end
+
+
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (1, 1);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (1, 2);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (1, 4);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (2, 1);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (2, 3);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (2, 5);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (2, 8);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (2, 9);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (3, 1);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (3, 2);')
+		#$db.query('SELECT * from customers_items;')
+		$db.query("SELECT * FROM customers, customers_items, items WHERE  customers.id = customers_items.customer_id AND customers_items.item_id = items.id;") #just for FOURTH TEST
+
 end
 
-def mysql2_adapter(record)
-	begin
-		db = Mysql2::Client.new(:host => @server, :username => @username, :password => @password, :database => @database)
-		db.query("INSERT INTO tvshows (show_name) values ('#{record}')")
-	ensure
-		db.close if db
-	end
+def mysql2_adapter()
+		# create the object and establish connection
+		if ($db).nil?
+			$db = Mysql2::Client.new(:host => @server, :username => @username, :password => @password, :database => @database)
+		end
+=begin FIRST TEST
+		$db.query("INSERT INTO customers (email, name) values ('test@test.com', 'pepaX');")
+		$db.query("UPDATE customers SET email='test1@test.com', name='pepaY' WHERE name='pepaX';")
+		$db.query("SELECT * FROM customers WHERE name='pepaY';")
+		$db.query("DELETE FROM customers WHERE name='pepaY';")
+=end
+
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (1, 1);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (1, 2);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (1, 4);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (2, 1);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (2, 3);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (2, 5);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (2, 8);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (2, 9);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (3, 1);')
+		$db.query('INSERT INTO customers_items(customer_id, item_id) values (3, 2);')
+		#$db.query('SELECT * from customers_items;')
+		$db.query("SELECT * FROM customers, customers_items, items WHERE  customers.id = customers_items.customer_id AND customers_items.item_id = items.id;") #just for FOURTH TEST
+
 end
 
-def dbi_adapter(record)
-	begin
-		db = DBI.connect("DBI:Mysql:integrace:localhost", "root", "")
-		db.do("INSERT INTO tvshows (show_name) values ('#{record}')")
-	ensure
-		db.disconnect if db
-	end
+def dbi_adapter()
+		# create the object and establish connection
+		if ($db).nil?
+			$db = DBI.connect("DBI:Mysql:soap:localhost", "root", "")
+		end
+=begin FIRST TEST
+		$db.do("INSERT INTO customers (email, name) values ('test@test.com', 'pepaX');")
+		$db.do("UPDATE customers SET email='test1@test.com', name='pepaY' WHERE name='pepaX';")
+		$db.do("SELECT * FROM customers WHERE name='pepaY';")
+		$db.do("DELETE FROM customers WHERE name='pepaY';")
+=end
+
+
+   	    #db.do('TRUNCATE TABLE customers_items;') #commmented out during the third test
+		$db.do('INSERT INTO customers_items(customer_id, item_id) values (1, 1);')
+		$db.do('INSERT INTO customers_items(customer_id, item_id) values (1, 2);')
+		$db.do('INSERT INTO customers_items(customer_id, item_id) values (1, 4);')
+		$db.do('INSERT INTO customers_items(customer_id, item_id) values (2, 1);')
+		$db.do('INSERT INTO customers_items(customer_id, item_id) values (2, 3);')
+		$db.do('INSERT INTO customers_items(customer_id, item_id) values (2, 5);')
+		$db.do('INSERT INTO customers_items(customer_id, item_id) values (2, 8);')
+		$db.do('INSERT INTO customers_items(customer_id, item_id) values (2, 9);')
+		$db.do('INSERT INTO customers_items(customer_id, item_id) values (3, 1);')
+		$db.do('INSERT INTO customers_items(customer_id, item_id) values (3, 2);')
+		#$db.do('SELECT * from customers_items;')
+		$db.do("SELECT * FROM customers, customers_items, items WHERE  customers.id = customers_items.customer_id AND customers_items.item_id = items.id;") #just for FOURTH TEST
 end
 
-def active_record_adapter(record)
-	begin
-		db = ActiveRecord::Base.establish_connection(:adapter => @adapter, :host => @server, :database => @database) 
-		#Tvshow.create(:movie_name => show_name)
-		db.connection.execute("INSERT INTO tvshows (show_name) values ('#{record}')")
-	ensure
-		db.disconnect! if db
-	end
+def active_record_adapter()
+		# create the object and establish connection
+		if ($db).nil?
+			$db = ActiveRecord::Base.establish_connection(:adapter => @adapter, :host => @server, :database => @database)
+		end
+=begin FIRST TEST
+		# insert
+		user = Customer.create(email: 'test@test.com', name: 'pepaX')
+		user.save
+		# update
+		user = Customer.find_by(name: 'pepaX')
+		user.name = 'pepaY'
+		user.save
+		# select
+		user = Customer.find_by(name: 'pepaY')
+		# delete
+		user = Customer.find_by(name: 'pepaY')
+		user.destroy
+=end
+
+		#db.connection.execute("INSERT INTO tvshows (show_name) values ('#{record}')")
+		cus_item = Customers_item.create(customer_id: 1, item_id: 1)
+		cus_item.save
+		cus_item = Customers_item.create(customer_id: 1, item_id: 2)
+		cus_item.save
+		cus_item = Customers_item.create(customer_id: 1, item_id: 4)
+		cus_item.save
+		cus_item = Customers_item.create(customer_id: 2, item_id: 1)
+		cus_item.save
+		cus_item = Customers_item.create(customer_id: 2, item_id: 3)
+		cus_item.save
+		cus_item = Customers_item.create(customer_id: 2, item_id: 5)
+		cus_item.save
+		cus_item = Customers_item.create(customer_id: 2, item_id: 8)
+		cus_item.save
+		cus_item = Customers_item.create(customer_id: 2, item_id: 9)
+		cus_item.save
+		cus_item = Customers_item.create(customer_id: 3, item_id: 1)
+		cus_item.save
+		cus_item = Customers_item.create(customer_id: 3, item_id: 2)
+		cus_item.save
+		# select
+		#cus_items = Customers_item.all()
+		Customer.joins(:customers_items, :items)
+
 end
 
 # ============== settings ==============
 # variables for repetitions
-no_of_repetitions = 2
-no_of_connections = 1000
+no_of_connections = 15
+no_of_repetitions = 100
 # Possibilities 'mysql_adapter', 'mysql2_adapter', 'dbi_adapter', 'active_record_adapter'
-adapter = 'mysql_adapter'
+adapter = 'active_record_adapter'
 # ============== end of settings ==============
+
+$db = nil
+results = Array.new	
+
+no_of_connections.times do
+	start = Time.now
+
+	no_of_repetitions.times do
+		send(adapter.to_s)
+	end
+
+	$db = nil if $db
+
+	finish = Time.now
+	puts (finish - start)
+	results << (finish - start)
+end
+
+# Saves results to a csv file
+File.open('results.csv', 'w+') do |file|
+	results.each do |result|
+		file.puts result.to_s
+	end
+end
+
+
+
+=begin NOT USED IN TESTS (it's just a snippet)
+require 'net/http' # gem for http communication (included in ruby)
+require 'json'     # gem for parsing json (gem install json)
 
 # Fetches JSON string with tv shows,
 # parse it to ruby hash and then creates array with 10 movies
@@ -95,12 +225,8 @@ no_of_repetitions.times do
 	results << (finish - start)
 end
 
-# Saves results to a csv file
-File.open('results.csv', 'w+') do |file|
-	results.each do |result|
-		file.puts result.to_s
-	end
-end
+
+=end
 
 
 
